6359bf9d6e08409e93417d9bd2d89c3c18d237c6
Jstein77 <Jordan.Stein77@gmail.com>
2018-10-03 18:50:07 -0700

    Add route test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-
+  test "should get route" do
+    get root_url
+    assert_response :success
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -27,6 +30,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
-  end 
+  end
 
 end
